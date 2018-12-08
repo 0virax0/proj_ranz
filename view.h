f1f507a469e0b1cfb206a0a8d33b9407fdcb2fa4
0virax0 <filippovis@outlook.it>
2018-12-08 19:22:09 +0100

    added container template

---
+++ view.h
@@ -0,0 +1,22 @@
+#ifndef VIEW_H
+#define VIEW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // VIEW_H
