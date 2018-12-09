f54bccd9aac1a1661f1d6336241010bed784b9d1
0virax0 <filippovis@outlook.it>
2018-12-09 15:59:38 +0100

    added Particle

--- model.h
+++ model.h
@@ -2,11 +2,12 @@
 #define MODEL_H
 #include <vector>
 template<class T, int dim>
-class Tree{
+class Tree{     //albero n-dimensionale di ricerca
 protected:
     class Node{
     public:
         T* data;
+        float position[dim];
         Node* children[dim];
 
         Node();
@@ -33,23 +34,49 @@ public:
 
         T* operator*();                     //ritorno il T* per renderlo nullable
     };
-protected: Iterator r; //primo nodo radice
-public:
+    protected: Iterator r; /*primo nodo radice*/ public:
     Iterator& root();
 
     Tree();
     Tree(const Tree& t);    //copia profonda
     Tree& operator=(const Tree& t); //assegnazione profonda
 
+    Iterator insert(const T& t, const float newPos[dim]);
+    Iterator move(Iterator m, const float newPos[dim]);
+    bool del(Iterator d);
+    Iterator findNearest(const float Pos[dim]);
+
     ~Tree();    //distruzione profonda
 };
 
 template<class T, int dim>
-class NearTree : public Tree<T, dim>{   //definisce il concetto di distanza euclidea
+class NearTree : public Tree<T, dim>{
 protected:
-    static std::vector<T>& findNrecursive(std::vector<T>& v, float targetPos[dim], int limit, typename Tree<T,dim>::Iterator it);
+    static std::vector<T>& findNrecursive(std::vector<T>& v, float targetPos[dim], int maxDistance, typename Tree<T,dim>::Iterator it, const float bounds[dim]);
 public:
-    std::vector<T>& findNeighbouring(std::vector<T>& v, float targetPos[dim], int limit, typename Tree<T,dim>::Iterator it = Tree<T,dim>::root()) const;
+    std::vector<T>& findNeighbouring(std::vector<T>& v, float targetPos[dim], int maxDistance, typename Tree<T,dim>::Iterator it = Tree<T,dim>::root()) const;
+};
+
+class Particle2{
+public:
+   class Properties{
+   public:
+        float position[2];
+        float velocity[2];
+        float pressure;
+        float temperature;
+   };
+   Properties* properties;
+
+
+
+   Particle2(); //create properties in the heap
+   Particle2(const Particle2&);
+   virtual ~Particle2();
+
+protected:
+   Properties* newProperties;
+
 };
 
 #endif // MODEL_H
